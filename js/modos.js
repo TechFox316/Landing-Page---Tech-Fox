f547736aac15f6bb3cb4c87dd64d63bc5ecc50ff
Diego Gabriel Schneider <92691671+Diego457923@users.noreply.github.com>
2021-12-30 18:46:42 -0300

    mapa rotas

--- js/modos.js
+++ js/modos.js
@@ -15,10 +15,12 @@ function trocaModos () {
         textoModos.innerHTML = "O MODO DE JOGO MAIS JOGADO - sua rota, mergulhe em épicos confrontos de equipe 5v5 e destrua o Nexus inimigo antes que destruam o seu";
         imagemModos.src = "img/summoners-rift.jpeg";
         tituloModos.innerHTML = " SUMMONERS RIFT"
+        tituloModos.style = "position: absolute; left: 34%;";
     } else {
         textoModos.innerHTML = "TODOS ALEATÓRIOS NA ROTA DO MEIO - Em uma ponte congelante, batalhe com sua equipe de Campeões aleatórios avançando em direção ao Nexus inimigo nesse divertido e caótico modo de jogo 5v5.";
         imagemModos.src = "img/aram.jpeg";
         tituloModos.innerHTML = " ARAM"
+        tituloModos.style = "position: absolute; left: 45%";
     }
    cont++;
 }
