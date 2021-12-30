0815de4660c2f77e39693b836ac14a229db90697
Giovanna Alyssa Marques da Silva <gih.alyssa15@gmail.com>
2021-12-30 19:53:46 -0300

    commit

---
+++ js/dowload.js
@@ -0,0 +1,6 @@
+var span= document.querySelector(".span");
+var botaoDownload= document.querySelector(".botao-download");
+
+botaoDownload.addEventListener("click", () => {
+    span.innerHTML= "Seu Download começará em instantes!";
+});
