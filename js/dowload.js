e6c85f6b6a860f3a923ac4da28076dbe81b6050f
Giovanna Alyssa Marques da Silva <gih.alyssa15@gmail.com>
2021-12-30 22:13:02 -0300

    Alteração doBotão do site, criação de pop-up do novo botão e o espaçamento entre o botão de download e do site.

--- js/dowload.js
+++ js/dowload.js
@@ -1,6 +1,14 @@
-var span= document.querySelector(".span");
-var botaoDownload= document.querySelector(".botao-download");
 
-botaoDownload.addEventListener("click", () => {
-    span.innerHTML= "Seu Download começará em instantes!";
+var frases = ["Seu Download começará em instantes!", "Aguarde...Você está sendo redirecionado"];
+function popUp(botao, frase) {
+    var span = document.querySelector(".span");
+    var botao = document.querySelector(botao);
+
+    botao.addEventListener("click", () => {
+        span.innerHTML= frases[frase];
 });
+}
+
+popUp(".botao-download", 0 );
+
+popUp(".botao-site", 1 );
