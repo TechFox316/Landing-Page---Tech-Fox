5bb17980333fbbaa95e4df71ff57615f30a992f7
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2021-12-31 17:29:40 -0300

    Mudanças fonte

--- js/dowload.js
+++ js/dowload.js
@@ -1,5 +1,5 @@
 
-var frases = ["Seu Download começará em instantes!", "Aguarde...Você está sendo redirecionado"];
+var frases = ["Seu Download começará em instantes!", "Aguarde... Você está sendo redirecionado!"];
 function popUp(botao, frase) {
     var span = document.querySelector(".span");
     var botao = document.querySelector(botao);
