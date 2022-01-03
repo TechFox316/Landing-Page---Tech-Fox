fafde2edb3169018c8143180a198cbf48629f306
Diego Gabriel Schneider <92691671+Diego457923@users.noreply.github.com>
2022-01-03 17:09:35 -0300

    final

--- js/script.js
+++ js/script.js
@@ -1,50 +1,55 @@
-var texto = document.querySelector(".texto")
+var textoHabilidades = document.querySelector(".texto")
+
+var tituloHabilidades = document.querySelector(".titulo-habilidades");
 
 var video = document.querySelector("#video-classe");
 
-var assasino = document.querySelector("#classe-assasino");
+var videos = ["video/assassino.mp4", "video/mago.mp4", "video/lutador.mp4", "video/atirador.mp4", "video/suporte.mp4", "video/tanque.mp4"]
 
-var mago = document.querySelector("#classe-mago");
+var textosHabilidades = [
+    "Excelente mobilidade e uma grande força para causar dano de forma rápida. Em contrapartida, esses Campeões podem ser mortos com a mesma velocidade em que são capazes de causar dano.",
+    "Campeões capazes de lançar feitiços mágicos para atrapalhar os inimigos. Suas defesas não são das melhores.",
+    "Personagens capazes de agradar diversos tipos de jogadores. Atuam bem tanto no ataque, como também na defesa. Seus ataques são de curta distância, mas essa exposição ao perigo é compensada com uma boa dose de pontos de resistência.",
+    "Causam muito dano sem ficar cara a cara com o perigo. Além da alta capacidade de “machucar” o inimigo, os Atiradores ajudam bastante na hora de detonar com alvos importantes do mapa. Entretanto são frágeis e precisam estar sempre protegidos pelo seu time.",
+    "Esses personagens lançam mão de ferramentas de proteção e apoio bastante úteis, de forma que esses Campeões são fundamentais para o sucesso da equipe.",
+    "São utilizados mais como ferramenta de controle do que para causar grandes danos nos inimigos, pois têm uma boa base defensiva, ajudando em situações específicas dentro da partida. Em compensação, sua mobilidade é baixa."
+];
 
-var lutador = document.querySelector("#classe-lutador");
+var titulosHabilidades = ["Assasino", "Mago", "Lutador", "Atirador", "Suporte","Tanque"];
 
-var atirador = document.querySelector("#classe-atirador");
+let setas = document.querySelectorAll(".button-habilidades");
 
-var suporte = document.querySelector("#classe-suporte");
+let classe = 0;
 
-var tanque = document.querySelector("#classe-tanque");
-
-
-var escolha = document.querySelector(".inputs")
-
-escolha.addEventListener("click", textoPagina)
-
-function textoPagina() {
-    if (assasino.checked) {
-        texto.innerHTML = "Excelente mobilidade e uma grande força para causar dano de forma rápida. Em contrapartida, esses Campeões podem ser mortos com a mesma velocidade em que são capazes de causar dano.";
-        video.setAttribute("src", "video/assassino.mp4")
-    } else if (mago.checked) {
-        texto.innerHTML = "Campeões capazes de lançar feitiços mágicos para atrapalhar os inimigos. Suas defesas não são das melhores.";
-        video.src = "video/mago.mp4";
-    }else if (lutador.checked) {
-        texto.innerHTML = "Personagens capazes de agradar diversos tipos de jogadores. Atuam bem tanto no ataque, como também na defesa. Seus ataques são de curta distância, mas essa exposição ao perigo é compensada com uma boa dose de pontos de resistência.";
-        video.src = "video/lutador.mp4";
-    }else if (atirador.checked) {
-        texto.innerHTML = "Causam muito dano sem ficar cara a cara com o perigo. Além da alta capacidade de “machucar” o inimigo, os Atiradores ajudam bastante na hora de detonar com alvos importantes do mapa. Entretanto são frágeis e precisam estar sempre protegidos pelo seu time.";
-        video.src = "video/atirador.mp4";
-    }else if (suporte.checked) {
-        texto.innerHTML = "Esses personagens lançam mão de ferramentas de proteção e apoio bastante úteis, de forma que esses Campeões são fundamentais para o sucesso da equipe.";
-        video.src = "video/suporte.mp4";
-    }else if (tanque.checked) {
-        texto.innerHTML = "São utilizados mais como ferramenta de controle do que para causar grandes danos nos inimigos, pois têm uma boa base defensiva, ajudando em situações específicas dentro da partida. Em compensação, sua mobilidade é baixa.";
-        video.src = "video/tanque.mp4";
-    }
+function mudaClasse(seta, cont) {
+    tituloHabilidades.innerHTML = titulosHabilidades[classe];
+    textoHabilidades.innerHTML = textosHabilidades[classe];
+    video.src = videos[classe];
+    setas[seta].addEventListener("click", () => {
+        troca(cont);
+    })
 }
 
-var classes = [assasino, mago, lutador, atirador, suporte, tanque];
+function troca(cont) {
+    if (classe + cont < 0) {
+        classe = titulosHabilidades.length-1;
+    }else if (classe + cont == titulosHabilidades.length) {
+        classe = 0;
+    }else {
+        classe += cont;
+    }
+    tituloHabilidades.innerHTML = titulosHabilidades[classe];
+    textoHabilidades.innerHTML = textosHabilidades[classe];
+    video.src = videos[classe];
+}
 
-var cont = 0;
+mudaClasse(0, -1)
+mudaClasse(1, 1)
 
+setInterval(() => {
+    troca(1);
+}, 5000);
+/*
 function troca() {
     classes[cont].checked = true;
     textoPagina();
@@ -57,5 +62,5 @@ troca();
 setInterval(() => {
     troca();
 }, 5000);
-
+*/
 
