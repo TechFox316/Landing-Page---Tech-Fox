90a9aacc8f42a1712892da14eda8849bcee0846d
Diego Gabriel Schneider <92691671+Diego457923@users.noreply.github.com>
2022-01-04 21:49:49 -0300

    ajustes duvidas e sobre

--- js-site/duvidas.js
+++ js-site/duvidas.js
@@ -4,11 +4,48 @@ let duvidas = document.querySelectorAll(".perguntas");
 function resposta(duvida) {
     var resposta = duvidas[duvida].querySelector(".resposta");
     var titulo = duvidas[duvida].querySelector(".resposta-titulo");
+    var podeAparecer = true;
     duvidas[duvida].addEventListener("click", () => {
-        resposta.classList.add("on");
-        titulo.classList.add("on");
-        //duvidas[duvida].classList.add("on");
+        if (podeAparecer) {
+            resposta.classList.add("on");
+            titulo.classList.add("on");
+        } else {
+            resposta.classList.remove("on");
+            titulo.classList.remove("on");
+        }
+        podeAparecer = !podeAparecer;
+
     })
 }
 
-resposta(0);
+for (let x = 0; x < duvidas.length; x++) {
+    resposta(x);
+    
+}
+
+/*
+function limpaImgem(limpa) {
+    if (limpa != rotas.length) {
+        rotas[limpa].classList.remove("on")
+        rotas[limpa].style.transition = "1s";
+    }
+}
+let rotaAntiga = rotas.length;
+
+
+function trocaRota(rota) {
+    rotas[rota].addEventListener("click", () => { 
+        limpaImgem(rotaAntiga);
+        if (rota != rotaAntiga) {
+            titulo.innerHTML = titulos[rota];
+            conteudo.innerHTML = textos[rota];
+            rotas[rota].classList.add("on");
+            rotaAntiga = rota;
+        } else {
+            titulo.innerHTML = titulos[5];
+            conteudo.innerHTML = textos[5];
+            rotaAntiga = rotas.length;
+        }
+    }) 
+}
+*/
